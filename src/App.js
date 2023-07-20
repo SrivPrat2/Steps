8e2147efa116c70bde54491eea03d092ab3b7e82
SrivPrat2 <srivastavapratyush72@gmail.com>
2023-07-20 18:03:41 +0530

    Using event handlers for click functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ const messages = [
 export default function App() {
   const step = 1;
 
+  function handlePrevious() {
+    alert("Prev");
+  }
+
+  function handleNext() {
+    alert("Next");
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -19,10 +27,16 @@ export default function App() {
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950F2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950F2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950F2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950F2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
